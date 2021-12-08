01352a9c0865981fa28e8da27336ea37612bf0d6
Belyankin Sergey <beljankin@yandex.ru>
2021-12-08 22:52:35 +0300

    SpringMVC_CRUD русская кодировка

--- src/main/java/ru/beljankin/spring/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/ru/beljankin/spring/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,8 +1,10 @@
 package ru.beljankin.spring.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -28,6 +30,15 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
         registerHiddenFieldFilter(aServletContext);
+        // кодировка кириллицы
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic filterRegistration = aServletContext
+                .addFilter("characterEncodingFilter", characterEncodingFilter);
+        filterRegistration.addMappingForUrlPatterns(null, false, "/*");
+
     }
 
     private void registerHiddenFieldFilter(ServletContext aContext) {
