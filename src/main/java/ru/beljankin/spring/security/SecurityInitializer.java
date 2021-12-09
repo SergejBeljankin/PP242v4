47f6e520a6c520075046fa19c0947eb3a05ef40b
SergejBeljankin <beljankin@yandex.ru>
2021-12-09 14:50:38 +0300

    CRUD + Security

---
+++ src/main/java/ru/beljankin/spring/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.beljankin.spring.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
+}
